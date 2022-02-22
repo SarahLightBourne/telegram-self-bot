6af9878ed486e53958037102a80d90b359805f59
SarahLightBourne <SaraLightBourne@gmail.com>
2022-02-22 16:39:51 +0300

    Target language changeable

--- source/translator/handlers.py
+++ source/translator/handlers.py
@@ -1,3 +1,4 @@
+from .config import TARGET_LANGUAGE
 from ..settings import telegram_clinet
 from .translate_message import translate_message
 
@@ -17,3 +18,14 @@ async def message_new(event: NewMessage.Event) -> None:
 async def message_edit(event: MessageEdited.Event) -> None:
   message: Message = event.message
   await translate_message(message)
+
+
+@telegram_clinet.on(events.NewMessage(func=lambda e: e.message.text.startswith('/target')))
+async def target_command(event: NewMessage.Event) -> None:
+  global TARGET_LANGUAGE
+
+  message: Message = event.message
+  message_text = message.text.replace('/target ', '')
+
+  TARGET_LANGUAGE = message_text
+  await message.delete()
