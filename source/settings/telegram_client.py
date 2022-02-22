c95590e72686e1938c52735b8b55a527fadba3fd
SarahLightBourne <SaraLightBourne@gmail.com>
2022-02-22 15:28:32 +0300

    Update telegram_client.py

--- source/settings/telegram_client.py
+++ source/settings/telegram_client.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient, events
+from telethon import TelegramClient
 from .settings import API_ID, APP_HASH, SESSION_PATH
 
 telegram_clinet = TelegramClient(str(SESSION_PATH), API_ID, APP_HASH)
